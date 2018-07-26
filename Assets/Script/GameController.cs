ad505c4739f57828679d55cad3ff846998ebfa30
Chaiyatad <chaiyatad@gmail.com>
2018-07-26 21:53:17 +0700

    Add spawn food code

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -4,16 +4,74 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public bool isNoFood = false;
+    public GameObject Food;
+
+    private GameObject snakes;
+    private SnakeController snakeController;
+    private List<Vector3> snakeLocation;
+    
+
 	// Use this for initialization
 	void Start () {
-		
+        snakes = GameObject.Find("Snakes");
+        snakeController = snakes.gameObject.GetComponent<SnakeController>();
 	}
-	
-	
+
+    void Update()
+    {
+        if (isNoFood) {
+            isNoFood = false;
+            Debug.Log("is no food");
+
+            AddFood();
+        }
+    }
+
+
     public static void Lose()
     {
         Time.timeScale = 0;
 
         Debug.Log(GameObject.Find("Snakes").transform.childCount);
+    }
+
+    private void AddFood()
+    {
+        float xPosition = 0;
+        float yPosition = 0;
+        bool isFinish = false;
+
+        snakeLocation = snakeController.snakeLocation;
+        while (!isFinish)
+        {
+            xPosition = Random.Range(-5, 5) + 0.5f ;
+            Debug.Log(xPosition + "x");
+            for(int i = 0; i<snakeLocation.Count; i++)
+            {
+                if(xPosition == snakeLocation[i].x)
+                {
+                    break;
+                }
+            }
+            isFinish = true;
+        }
+
+        isFinish = false;
+        while (!isFinish)
+        {
+            yPosition = Random.Range(-5, 5) + 0.5f;
+            for (int i = 0; i < snakeLocation.Count; i++)
+            {
+                if (yPosition == snakeLocation[i].y)
+                {
+                    break;
+                }
+            }
+            isFinish = true;
+        }
+
+        Instantiate(Food, new Vector3(xPosition, yPosition, 0),new Quaternion());
+
     }
 }
--- Assets/Script/SnakeBehavior.cs
+++ Assets/Script/SnakeBehavior.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class SnakeBehavior : MonoBehaviour {
 
     private GameObject snakes;
+    private GameObject gameController;
+
     private SnakeController snakeController;
+    private GameController gameControllerScript;
 
 	void Start () {
         snakes = GameObject.Find("Snakes");
         snakeController = snakes.GetComponent<SnakeController>();
+
+        gameController = GameObject.Find("GameController");
+        gameControllerScript = gameController.GetComponent<GameController>();
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,6 +27,7 @@ public class SnakeBehavior : MonoBehaviour {
         else if (collision.gameObject.CompareTag("Food"))
         {
             snakeController.isEating = true;
+            gameControllerScript.isNoFood = true;
             Destroy(collision.gameObject);
         }
     }
